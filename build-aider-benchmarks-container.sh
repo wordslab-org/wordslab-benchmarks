0b676297bd12597a55a1901ebfde74776d256819
Laurent Prud'hon <laurent.prudhon@hotmail.fr>
2025-05-11 22:19:54 +0200

    First results for csharp

--- build-aider-benchmarks-container.sh
+++ build-aider-benchmarks-container.sh
@@ -7,6 +7,7 @@ mkdir tmp.benchmarks
 
 # Clone the repo with the exercises
 git clone https://github.com/exercism/python tmp.benchmarks/python-benchmark/python
+git clone https://github.com/exercism/csharp tmp.benchmarks/csharp-benchmark/csharp
 git clone https://github.com/Aider-AI/polyglot-benchmark tmp.benchmarks/polyglot-benchmark
 
 # Build container
